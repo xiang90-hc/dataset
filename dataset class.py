9dce6d27023911a6dd95f895f48038ade737a699
xiang90-hc <hancheng710@gmail.com>
2025-03-04 17:46:39 -0500

    Update dataset class.py

--- dataset class.py
+++ dataset class.py
@@ -1,13 +1,12 @@
 import pandas as pd
+import pyreadr
 
 class DataSet:
-    def __init__(self, path, target):
+    def __init__(self, path):
         self.filepath = path
-        self.dataset = pd.read_csv(self.filepath)
-        self.target_var = target
-        self.covariates = list(self.dataset)
-        for variables in self.target_var:
-            self.covariates.remove(variables)
+        self.dataset = pd.DataFrame([])
+        self.target_var = []
+        self.covariates = []
 
     def __len__(self):
         return self.dataset.shape[0]
@@ -20,3 +19,22 @@ class DataSet:
 
     def get_covariate(self):
         return self.dataset[self.covariates]
+
+class ACICDataSet(DataSet):
+    def __init__(self, path, target):
+        super(ACICDataSet, self).__init__(path)
+        dataset = pyreadr.read_r(self.filepath)
+        self.dataset = dataset[list(dataset.keys())[0]]
+        self.target_var = target
+        self.covariates = list(self.dataset)
+        for variables in self.target_var:
+            self.covariates.remove(variables)
+
+class MIMICDataSet(DataSet):
+    def __init__(self, path, target):
+        super(MIMICDataSet, self).__init__(path)
+        self.dataset = pd.read_csv(self.filepath)
+        self.target_var = target
+        self.covariates = list(self.dataset)
+        for variables in self.target_var:
+            self.covariates.remove(variables)
