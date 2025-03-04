cab58fc4b5ccd989b60752fa6446a27d078af354
xiang90-hc <hancheng710@gmail.com>
2025-03-04 15:27:09 -0500

    Update dataset class.py

--- dataset class.py
+++ dataset class.py
@@ -1,9 +1,13 @@
 import pandas as pd
 
 class DataSet:
-    def __init__(self, path):
+    def __init__(self, path, target):
         self.filepath = path
         self.dataset = pd.read_csv(self.filepath)
+        self.target_var = target
+        self.covariates = list(self.dataset)
+        for variables in self.target_var:
+            self.covariates.remove(variables)
 
     def __len__(self):
         return self.dataset.shape[0]
@@ -11,19 +15,16 @@ class DataSet:
     def __getitem__(self, index):
         return self.dataset.iloc[index]
 
-
-class CovDataSet(DataSet):
-    def __init__(self, path, target):
-        super(CovDataSet, self).__init__(path)
-        self.target_var = target
-
     def get_target(self):
         return self.dataset[self.target_var]
 
     def get_covariate(self):
+        return self.dataset[self.covariates]
+
+    def get_covariate(self):
         index = list(self.dataset)
-        target_index = []
         for variables in self.target_var:
             index.remove(variables)
 
         return self.dataset[index]
+
